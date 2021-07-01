37a3dc7a35763e2af8b0a6d61b39bc9609d1e18f
Sean Aldridge Francisco <seanaldridgemfrancisco@iskolarngbayan.pup.edu.ph>
2021-07-01 13:51:31 +0800

    Add project files.

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿
+namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnl1 = new System.Windows.Forms.Panel();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.pnl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnl1
+            // 
+            this.pnl1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pnl1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pnl1.Controls.Add(this.btn9);
+            this.pnl1.Controls.Add(this.btnRestart);
+            this.pnl1.Controls.Add(this.btn7);
+            this.pnl1.Controls.Add(this.btn8);
+            this.pnl1.Controls.Add(this.btn6);
+            this.pnl1.Controls.Add(this.btn5);
+            this.pnl1.Controls.Add(this.btn4);
+            this.pnl1.Controls.Add(this.btn3);
+            this.pnl1.Controls.Add(this.btn2);
+            this.pnl1.Controls.Add(this.btn1);
+            this.pnl1.Location = new System.Drawing.Point(12, 12);
+            this.pnl1.Name = "pnl1";
+            this.pnl1.Size = new System.Drawing.Size(245, 309);
+            this.pnl1.TabIndex = 0;
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(156, 158);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(65, 65);
+            this.btn9.TabIndex = 10;
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnRestart.Location = new System.Drawing.Point(14, 229);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(207, 65);
+            this.btnRestart.TabIndex = 9;
+            this.btnRestart.Text = "Restart";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(14, 158);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(65, 65);
+            this.btn7.TabIndex = 8;
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(85, 158);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(65, 65);
+            this.btn8.TabIndex = 7;
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(156, 87);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(65, 65);
+            this.btn6.TabIndex = 6;
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(85, 87);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(65, 65);
+            this.btn5.TabIndex = 5;
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(14, 87);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(65, 65);
+            this.btn4.TabIndex = 4;
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(156, 16);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(65, 65);
+            this.btn3.TabIndex = 3;
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(85, 16);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(65, 65);
+            this.btn2.TabIndex = 2;
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(14, 16);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(65, 65);
+            this.btn1.TabIndex = 1;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(271, 337);
+            this.Controls.Add(this.pnl1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "TicTacToe";
+            this.pnl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl1;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn1;
+    }
+}
+
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        String[] gameBoard = new string[9];
+        int current_Turn = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public String returnSymbol(int turn)
+        {
+            if (turn % 2 == 0)
+            {
+                return "O";
+            }
+            else
+            {
+                return "X";
+            }
+        }
+
+        public System.Drawing.Color determineColor(String symbol)
+        {
+            if (symbol.Equals("O"))
+            {
+                return System.Drawing.Color.LightGreen;
+            }
+            else if (symbol.Equals("X"))
+            {
+                return System.Drawing.Color.LightBlue;
+            }
+            return System.Drawing.Color.White;
+        }
+
+        public void checkWinConditions()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                String combination = "";
+                int one = 0, two = 0, three = 0;
+
+                switch (i)
+                {
+                    case 0:
+                        combination = gameBoard[0] + gameBoard[1] + gameBoard[2];
+                        one = 0;
+                        two = 1;
+                        three = 2;
+                        break;
+                    case 1:
+                        combination = gameBoard[3] + gameBoard[4] + gameBoard[5];
+                        one = 3;
+                        two = 4;
+                        three = 5;
+                        break;
+                    case 2:
+                        combination = gameBoard[6] + gameBoard[7] + gameBoard[8];
+                        one = 6;
+                        two = 7;
+                        three = 8;
+                        break;
+                    case 3:
+                        combination = gameBoard[0] + gameBoard[3] + gameBoard[6];
+                        one = 0;
+                        two = 3;
+                        three = 6;
+                        break;
+                    case 4:
+                        combination = gameBoard[1] + gameBoard[4] + gameBoard[7];
+                        one = 1;
+                        two = 4;
+                        three = 7;
+                        break;
+                    case 5:
+                        combination = gameBoard[2] + gameBoard[5] + gameBoard[8];
+                        one = 2;
+                        two = 5;
+                        three = 8;
+                        break;
+                    case 6:
+                        combination = gameBoard[0] + gameBoard[4] + gameBoard[8];
+                        one = 0;
+                        two = 4;
+                        three = 8;
+                        break;
+                    case 7:
+                        combination = gameBoard[2] + gameBoard[4] + gameBoard[6];
+                        one = 2;
+                        two = 4;
+                        three = 6;
+                        break;
+                }
+
+                if (combination.Equals("OOO"))
+                {
+                    change_Color(one);
+                    change_Color(two);
+                    change_Color(three);
+                    MessageBox.Show("O wins the game! " + "\r\n" + "GG ");
+                    reset();
+                }
+                else if (combination.Equals("XXX"))
+                {
+                    change_Color(one);
+                    change_Color(two);
+                    change_Color(three);
+                    MessageBox.Show("X wins the game! " + "\r\n" + "GG ");
+                    reset();
+                }
+
+                checkDrawConditions();
+            }
+        }
+
+        public void change_Color(int number)
+        {
+            switch (number)
+            {
+                case 0:
+                    btn1.BackColor = System.Drawing.Color.Red;
+                    break;
+                case 1:
+                    btn2.BackColor = System.Drawing.Color.Red;
+                    break;
+                case 2:
+                    btn3.BackColor = System.Drawing.Color.Red;
+                    break;
+                case 3:
+                    btn4.BackColor = System.Drawing.Color.Red;
+                    break;
+                case 4:
+                    btn5.BackColor = System.Drawing.Color.Red;
+                    break;
+                case 5:
+                    btn6.BackColor = System.Drawing.Color.Red;
+                    break;
+                case 6:
+                    btn7.BackColor = System.Drawing.Color.Red;
+                    break;
+                case 7:
+                    btn8.BackColor = System.Drawing.Color.Red;
+                    break;
+                case 8:
+                    btn9.BackColor = System.Drawing.Color.Red;
+                    break;
+            }
+        }
+
+        public void checkDrawConditions()
+        {
+            int counter = 0;
+            for (int i = 0; i < gameBoard.Length; i++)
+            {
+                if (gameBoard[i] != null) { counter++; }
+
+                if (counter == 9)
+                {
+                    MessageBox.Show("The game is a draw! " + "\r\n" + "WP ");
+                    reset();
+                }
+            }
+        }
+
+        public void reset()
+        {
+            btn1.Text = "";
+            btn2.Text = "";
+            btn3.Text = "";
+            btn4.Text = "";
+            btn5.Text = "";
+            btn6.Text = "";
+            btn7.Text = "";
+            btn8.Text = "";
+            btn9.Text = "";
+
+            btn1.BackColor = System.Drawing.Color.White;
+            btn2.BackColor = System.Drawing.Color.White;
+            btn3.BackColor = System.Drawing.Color.White;
+            btn4.BackColor = System.Drawing.Color.White;
+            btn5.BackColor = System.Drawing.Color.White;
+            btn6.BackColor = System.Drawing.Color.White;
+            btn7.BackColor = System.Drawing.Color.White;
+            btn8.BackColor = System.Drawing.Color.White;
+            btn9.BackColor = System.Drawing.Color.White;
+            gameBoard = new string[9];
+            current_Turn = 0;
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            current_Turn++;
+            gameBoard[0] = returnSymbol(current_Turn);
+            Color button_Color = determineColor(gameBoard[0]);
+            btn1.BackColor = button_Color;
+            btn1.Text = gameBoard[0];
+            checkWinConditions();
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            current_Turn++;
+            gameBoard[1] = returnSymbol(current_Turn);
+            Color button_Color = determineColor(gameBoard[1]);
+            btn2.BackColor = button_Color;
+            btn2.Text = gameBoard[0];
+            checkWinConditions();
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            current_Turn++;
+            gameBoard[2] = returnSymbol(current_Turn);
+            Color button_Color = determineColor(gameBoard[2]);
+            btn3.BackColor = button_Color;
+            btn3.Text = gameBoard[2];
+            checkWinConditions();
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            current_Turn++;
+            gameBoard[3] = returnSymbol(current_Turn);
+            Color button_Color = determineColor(gameBoard[3]);
+            btn4.BackColor = button_Color;
+            btn4.Text = gameBoard[3];
+            checkWinConditions();
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            current_Turn++;
+            gameBoard[4] = returnSymbol(current_Turn);
+            Color button_Color = determineColor(gameBoard[4]);
+            btn5.BackColor = button_Color;
+            btn5.Text = gameBoard[4];
+            checkWinConditions();
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            current_Turn++;
+            gameBoard[5] = returnSymbol(current_Turn);
+            Color button_Color = determineColor(gameBoard[5]);
+            btn6.BackColor = button_Color;
+            btn6.Text = gameBoard[5];
+            checkWinConditions();
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            current_Turn++;
+            gameBoard[6] = returnSymbol(current_Turn);
+            Color button_Color = determineColor(gameBoard[6]);
+            btn7.BackColor = button_Color;
+            btn7.Text = gameBoard[6];
+            checkWinConditions();
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            current_Turn++;
+            gameBoard[7] = returnSymbol(current_Turn);
+            Color button_Color = determineColor(gameBoard[7]);
+            btn8.BackColor = button_Color;
+            btn8.Text = gameBoard[7];
+            checkWinConditions();
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            current_Turn++;
+            gameBoard[8] = returnSymbol(current_Turn);
+            Color button_Color = determineColor(gameBoard[8]);
+            btn9.BackColor = button_Color;
+            btn9.Text = gameBoard[8];
+            checkWinConditions();
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            reset();
+        }
+    }
+}
